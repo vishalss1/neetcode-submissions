79d5c15176617018698605acee30151b58901a1e
Vishal Shetagar <vishal.shetegar1@gmail.com>
2026-07-19 00:31:26 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        if(strs.size() == 0) return "";
+
+        string res = "";
+        for(string s: strs) {
+            res += to_string(s.size()) + "#" + s;
+        }
+
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+
+        int i = 0;
+        while(i < s.size()) {
+            int j = i;
+
+            while(s[j] != '#') 
+                j++;
+
+            int len = stoi(s.substr(i, j-i));
+
+            res.push_back(s.substr(j+1, len));
+
+            i = j + 1 + len;
+        }
+
+        return res;
+    }
+};
